1922c756086dd4fb6f609bd07598f8f0dc4b7176
kouhei1992 <113017322+kouhei1992@users.noreply.github.com>
2022-09-30 20:51:40 +0900

    課題提出テスト
    4章　バグ修正

---
+++ 4-1/test2/dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+// セッション開始
+// session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "339833";
+// データベース名
+$db['dbname'] = "YIGroupBlog";
+?>
